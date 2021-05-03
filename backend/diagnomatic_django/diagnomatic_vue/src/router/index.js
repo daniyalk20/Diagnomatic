df7a625ebb1d44a0eed4ad702ecba2aecd3af6e0
Daniyalatjit <Daniyalatjit@users.noreply.github.com>
2021-05-03 18:06:43 +0530

    Added APIs

--- backend/diagnomatic_django/diagnomatic_vue/src/router/index.js
+++ backend/diagnomatic_django/diagnomatic_vue/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import ABout from '../views/About.vue'
 const routes = [
   {
     path: '/',
@@ -14,6 +14,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "contact" */ '../views/Contact.vue')
   }
 ]
 
--- backend/diagnomatic_django/diagnomatic_vue/src/views/Home.vue
+++ backend/diagnomatic_django/diagnomatic_vue/src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
   <main role="main" class="inner cover">
+    <HelloWorld></HelloWorld>
     <h1 class="cover-heading">Cover your page.</h1>
     <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
     <p class="lead">
       <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
     </p>
+    <HelloWorld></HelloWorld>
   </main>
 </template>
 
@@ -15,6 +17,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'Home',
   components: {
+    HelloWorld
   }
 }
 </script>
